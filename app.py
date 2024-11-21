5b536fdf29c56ff66274bc35aaa2a8a246de4feb
zoro-22 <137077280+zoro-22@users.noreply.github.com>
2024-11-21 20:55:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition result:", add(2, 3))
+    print("Subtraction result:", subtract(5, 3))
