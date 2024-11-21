9c7195153a9b013afeac433d1d4ddf9e6bf86206
zoro-22 <137077280+zoro-22@users.noreply.github.com>
2024-11-21 22:30:34 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,9 +1,13 @@
 # test_your_script.py
-import pytest
+import unittest
 from app import add, subtract
 
-def test_add():
-    assert add(2, 3) == 5
+class test(unittest.TestCase):
+    
+    def test_add():
+        assert add(2, 3) == 5
 
-def test_subtract():
-    assert subtract(5, 3) == 2
+    def test_subtract():
+        assert subtract(5, 3) == 2
+if __name__=="__main__":
+    unittest.main()
